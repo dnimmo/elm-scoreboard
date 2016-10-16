3c9a5d5e80e4bc9f0de634a873b3dec5c3c41265
David Nimmo <dnimmo@gmail.com>
2016-10-16 19:13:37 +0100

    fixed formatting

--- Main.elm
+++ Main.elm
@@ -11,154 +11,159 @@ import Html.App as App
 
 
 type alias Model =
-  { players : List Player
-  , name : String
-  , playerId : Maybe Int
-  , plays : List Play
-  , nextId : Int
-  }
+    { players : List Player
+    , name : String
+    , playerId : Maybe Int
+    , plays : List Play
+    , nextId : Int
+    }
 
 
 type alias Player =
-  { id : Int
-  , name : String
-  , points : Int
-  }
+    { id : Int
+    , name : String
+    , points : Int
+    }
 
 
 type alias Play =
-  { id : Int
-  , playerId : Int
-  , name : String
-  , points : Int
-  }
+    { id : Int
+    , playerId : Int
+    , name : String
+    , points : Int
+    }
 
 
 initModel : Model
 initModel =
-  { players = []
-  , name = ""
-  , playerId = Nothing
-  , plays = []
-  , nextId = 0
-  }
+    { players = []
+    , name = ""
+    , playerId = Nothing
+    , plays = []
+    , nextId = 0
+    }
 
 
 type Msg
-  = Edit Player
-  | Score Player Int
-  | Input String
-  | Save
-  | Cancel
-  | DeletePlay Play
+    = Edit Player
+    | Score Player Int
+    | Input String
+    | Save
+    | Cancel
+    | DeletePlay Play
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Input name ->
-      { model | name = name }
+    case msg of
+        Input name ->
+            { model | name = name }
 
-    Save ->
-      if (String.isEmpty model.name) then
-        model
-      else
-        save model
+        Save ->
+            if (String.isEmpty model.name) then
+                model
+            else
+                save model
 
-    Cancel ->
-      { model | name = initModel.name
-              , playerId = Nothing
-      }
+        Cancel ->
+            { model
+                | name = initModel.name
+                , playerId = Nothing
+            }
 
-    _ ->
-      model
+        _ ->
+            model
+
 
 save : Model -> Model
 save model =
-  case model.playerId of
-    Just id ->
-      edit model id
+    case model.playerId of
+        Just id ->
+            edit model id
 
-    Nothing ->
-      add model
+        Nothing ->
+            add model
 
 
 add : Model -> Model
 add model =
-  let
-    player =
-      Player model.nextId model.name 0
+    let
+        player =
+            Player model.nextId model.name 0
 
-    newPlayers =
-      player :: model.players
-
-  in
-    { model
-        | players = newPlayers
-        , name = ""
-        , nextId = model.nextId + 1
-    }
+        newPlayers =
+            player :: model.players
+    in
+        { model
+            | players = newPlayers
+            , name = ""
+            , nextId = model.nextId + 1
+        }
 
 
 edit : Model -> Int -> Model
 edit model id =
-  let
-    newPlayers =
-      List.map (\player ->
-                  if player.id == id then
-                      { player | name = model.name }
-                  else
-                    player
-               )
-               model.players
-    newPlays =
-      List.map (\play ->
-                  if play.playerId == id then
-                        { play | name = model.name }
-                  else
-                      play
-                  )
-                  model.plays
+    let
+        newPlayers =
+            List.map
+                (\player ->
+                    if player.id == id then
+                        { player | name = model.name }
+                    else
+                        player
+                )
+                model.players
 
-  in
-    { model
-        | players = newPlayers
-        , plays = newPlays
-        , name = ""
-        , playerId = Nothing
-    }
+        newPlays =
+            List.map
+                (\play ->
+                    if play.playerId == id then
+                        { play | name = model.name }
+                    else
+                        play
+                )
+                model.plays
+    in
+        { model
+            | players = newPlayers
+            , plays = newPlays
+            , name = ""
+            , playerId = Nothing
+        }
+
+
 
 -- view
 
 
 view : Model -> Html Msg
 view model =
-  div [ class "scoreboard" ]
-      [ h1 [] [ text "Score Keeper" ]
-      , playerForm model
-      , p [] [ text (toString model) ]
-      ]
+    div [ class "scoreboard" ]
+        [ h1 [] [ text "Score Keeper" ]
+        , playerForm model
+        , p [] [ text (toString model) ]
+        ]
 
 
 playerForm : Model -> Html Msg
 playerForm model =
-  Html.form [ onSubmit Save ]
-      [ input
-          [ type' "text"
-          , placeholder "Add/Edit Player..."
-          , onInput Input
-          , value model.name
-          ]
-          []
-      , button [ type' "submit" ] [ text "Save" ]
-      , button [ type' "button", onClick Cancel ] [ text "Cancel" ]
-      ]
+    Html.form [ onSubmit Save ]
+        [ input
+            [ type' "text"
+            , placeholder "Add/Edit Player..."
+            , onInput Input
+            , value model.name
+            ]
+            []
+        , button [ type' "submit" ] [ text "Save" ]
+        , button [ type' "button", onClick Cancel ] [ text "Cancel" ]
+        ]
 
 
 main : Program Never
 main =
-  App.beginnerProgram
-    { model = initModel
-    , view = view
-    , update = update
-    }
+    App.beginnerProgram
+        { model = initModel
+        , view = view
+        , update = update
+        }
